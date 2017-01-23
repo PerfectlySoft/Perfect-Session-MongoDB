2cd29215e34396f60c374203fcd8af507d2d49f5
Kyle Jessup <badthing@gmail.com>
2017-01-23 11:59:09 -0500

    Updated for session API change.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PerfectSessionMongoDB",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
+        .Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 1),
     ]
 )
