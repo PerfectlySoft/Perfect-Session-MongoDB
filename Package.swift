1b741b94b8a5aaeeb57f5d40ae02bcc1b59c9c82
Jonathan Guthrie <jono@perfect.org>
2017-10-03 17:13:41 -0400

    update for swift 4 perfect 3

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 20:32:37 +0000
+// Date: 2017-10-03 21:06:06 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectSessionMongoDB",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 1),
-    ]
+	name: "PerfectSessionMongoDB",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
+		.Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 3),
+	]
 )
